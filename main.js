126af3d8d02d05c4b9b9f872d915a2225275fb73
itzmjauz <itzmjauz@gmail.com>
2014-09-11 18:50:05 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function shuffle(list,newlist){
+  while(list.length != 0){
+    index = Math.round(Math.random() * list.length)
+    newlist.push(list[index])
+    list.splice(index, 1)
+    shuffle(list, newlist)
+  }
+  if(list.length==0) return newlist.filter(Number)
+}
+
+function merge(list,num){
+  while(list.length!=0){
+    num.push(list.slice(0,3).join().replace(/,/g,''))
+    list.splice(0,3)
+    merge(list,num)
+  }
+  if(list.length==0) return num
+}
+
+function test(list){
+  if(list[1]/list[0]==2 && list[2]/list[0]==3) console.log(list)
+  else console.log(list + ' == nope')
+}
+
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+while(true) {
+  test(merge(shuffle(numbers,[]),[]))
+}
