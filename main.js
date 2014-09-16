a9673e89544bcb5703d98de41d9841f5f72425fa
itzmjauz <itzmjauz@gmail.com>
2014-09-16 12:10:01 +0200

    Improve code structure

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-function shuffle(list,newlist){
-  while(list.length != 0){
+function shuffle(list, newlist) {
+  while (list.length != 0){
     index = Math.round(Math.random() * list.length)
     newlist.push(list[index])
     list.splice(index, 1)
     shuffle(list, newlist)
   }
-  if(list.length==0) return newlist.filter(Number)
+  if (list.length == 0) return newlist.filter(Number)
 }
 
-function merge(list,num){
-  while(list.length!=0){
-    num.push(list.slice(0,3).join().replace(/,/g,''))
-    list.splice(0,3)
-    merge(list,num)
+function merge(list, num) {
+  while (list.length != 0) {
+    num.push(list.slice(0, 3).join().replace(/,/g, ''))
+    list.splice(0, 3)
+    merge(list, num)
   }
-  if(list.length==0) return num
+  if (list.length == 0) return num
 }
 
-function test(list){
-  if(list[1]/list[0]==2 && list[2]/list[0]==3) console.log(list)
+function test(list) {
+  if(list[1] / list[0] == 2 && list[2] / list[0] == 3) console.log(list)
 }
 
 
 //running
-function run(){
+function run() {
   setTimeout( function() {
-    nlist = shuffle([1,2,3,4,5,6,7,8,9],[])
-    nlist = merge(nlist,[])
+    nlist = shuffle([1,2,3,4,5,6,7,8,9], [])
+    nlist = merge(nlist, [])
     test(nlist)
     run()
-  },0)
+  }, 0)
 }
 run()
 
